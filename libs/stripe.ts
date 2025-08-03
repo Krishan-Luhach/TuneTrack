61648eeece957e10b4e43df06cc39af51274a767
Krishan-Luhach <krishan_11915055@nitkkr.ac.in>
2025-08-04 01:03:17 +0530

    chore: ts error removed

--- libs/stripe.ts
+++ libs/stripe.ts
@@ -3,7 +3,7 @@ import Stripe from 'stripe';
 export const stripe = new Stripe(
     process.env.STRIPE_SECRET_KEY ?? '',
     {
-        apiVersion:'2025-05-28.basil',
+        apiVersion:'2025-07-30.basil',
         appInfo:{
             name:'TuneTrack',
             version:'0.1.0'
